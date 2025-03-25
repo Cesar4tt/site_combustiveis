b52913ec7756b8e8ac7d7d1efec54d03b3d13c8b
César Alonso <isaquealosnso@gmail.com>
2025-03-25 01:13:48 -0300

    update

--- javaScript.js
+++ javaScript.js
@@ -1,7 +1,32 @@
-const selectElement = document.getElementById("porcentagem");
+importScripts (Math)
+const selectElement = document.getElementById("select_porcentagem");
 
-for (let i = 45; i <= 100; i++) {
-    const option = document.createElement("option");
-    option.textContent = `${i}%`;
-    selectElement.appendChild(option);
+function Calcular (){
+    let preco = document.getElementById('preco_cumbustível');
+    let valor = Number(preco.value);
+    let select = document.getElementById('select_porcentagem');
+    let valores = select.options [select.selectedIndex];
+    let porcentagem = valores.value;
+ 
+    let calculogasolina = (valor*porcentagem); 
+    let calculoetanol = (1/(valor*porcentagem) * 100);
+ 
+   // const totalgasolina = calculogasolina.toFixed(2);
+   // const totaletanol = calculoetanol.toFixed(2);
+    const calcgasolina = calculogasolina.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+    const calcetanol = calculoetanol.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+   
+    let resultado = document.getElementById('resultado');
+ 
+    if(document.getElementById('Gasolina').checked){
+      resultado.innerHTML = `Abasteça com ETANOL se seu preço for igual ou menor a: <br><br>${calcgasolina}`;
+    }
+ 
+    else if (document.getElementById('Etanol').checked){
+        resultado.innerHTML = `Abasteça com GASOLINA se seu preço for igual ou menor a: <br><br>${calcetanol}`;
+    }
+ 
+    else{
+        resultado.innerHTML = "Nenhum valor encontrado";
+    }
 }
